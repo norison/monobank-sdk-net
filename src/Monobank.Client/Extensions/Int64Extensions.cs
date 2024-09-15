a10ade3d38a94a9c92cb83361cbed91722c546c0
Ihor Matiev <ihor.matiev@gmail.com>
2024-09-15 18:14:14 +0300

    Added utc kind

--- src/Monobank.Client/Extensions/Int64Extensions.cs
+++ src/Monobank.Client/Extensions/Int64Extensions.cs
@@ -6,7 +6,7 @@ namespace Monobank.Client
     {
         public static DateTime ToDateTime(this long seconds)
         {
-            return new DateTime(1970, 1, 1).AddSeconds(seconds);
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(seconds);
         }
     }
 }
